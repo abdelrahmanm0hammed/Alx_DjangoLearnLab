beea1883776e181847ac2102966bcfa31dd049a4
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-08 19:58:18 +0200

    adding self.name

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,16 +3,20 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max)
+    return self.name
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)(max_length=100)
+    return self.title
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
+    return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    return self.name
