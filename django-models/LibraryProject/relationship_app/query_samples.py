9f181542d71ab8aef9342c117ef49d7e9273cbc6
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-08 20:33:40 +0200

    adding query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ from .models import Book , Author, Librarian, Library
 
 
 
-books = ["Author.objects.get(name=author_name)", "objects.filter(author=author)"]
+books = Book.objects.filter(author=Author.objects.get(name=author_name))
 all_books = ["Library.objects.get(name=library_name)", "books.all()"]
 librarian = ["Librarian.objects.get(library="]
 
