5169a1f65ae7add1ec14f2a49af5e375a3efdd7c
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-08 21:12:55 +0200

    adding query samples

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,20 +3,24 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max)
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)(max_length=100)
-    return self.title
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
-    return self.name
+    def __str__(self):
+        return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
-    return self.name
+    def __str__(self):
+        return self.name
