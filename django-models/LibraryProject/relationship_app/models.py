7e742b481023fc7caa2a33f235487d02e80a8d17
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-08 19:54:02 +0200

    converting def to class inside models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 # Create your models here.
-def Author(models.Model):
+class Author(models.Model):
     name = models.CharField(max)
 
 
-def Book(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)(max_length=100)
 
-def Library(models.Model):
+class Library(models.Model):
     name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
 
-def Librarian(models.Model):
+class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
