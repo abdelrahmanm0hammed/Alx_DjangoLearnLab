c449314375bbc71771f8aa0f2f887d34e8c415fb
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-09 22:56:01 +0200

    implement role-based access control in django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
@@ -24,3 +25,9 @@ class Librarian(models.Model):
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
+
+class UserProfile(models.Model):
+    user =models.OneToOneField(User,on_delete=models.CASCADE)
+    role = models.CharField(max_length=50,choices=[('Admin','Admin'),('Librarian','Librarian'),('Member','Member')])
+    def __str__(self):
+        return f"{self.user.username} ({self.role})"
