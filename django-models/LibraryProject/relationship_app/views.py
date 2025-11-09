13006b57af5cca1bad94b1645d34a228ac460ccf
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-09 21:33:57 +0200

    adding login, logout and register

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -31,7 +31,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'relationship_app/register.html'
+def register(request):
+    if request.method == "POST"
+        form =UserCreationForm(request.POST)
+        if form.is_valid():
+            user =form.save()
+            return redirect('login')
+        else:
+            form = UserCreationForm()
+        
+        return render(request, 'relationship_app/register.html',{'form':form})
+    
