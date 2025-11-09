3bd83d7a5749fbeddf7af9025a98766f0fb3b5b8
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-09 23:12:13 +0200

    implement role-based access control in django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .models import Author
 from .models import Library
