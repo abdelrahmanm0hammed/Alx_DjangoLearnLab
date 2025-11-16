f363c2a68c29285c012fe4e9ceb27a9b95b85e9f
AbdelrahmanMohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-16 22:27:33 +0200

    Fix import and formatting in views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required ,login_required
 from .models import Book
-
+from .forms import ExampleForm
 
 @login_required
 @permissin_required('bookshelf.can_view',raise_exception=True)
@@ -9,4 +9,5 @@ def book_list(request):
   books =Book.objects.all()
   return render(request,'bookshelf/book_list.html',{'books':books})
 # Create your views here.
+
 
