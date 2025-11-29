b8000927b9075c5e3205ef9f11ebb6091cdf3c17
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-29 18:24:05 +0200

    adding permission to restrict create, update and delete view to authenticated users only

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,25 +2,40 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book , Author
 from .serializers import BookSerializer , AuthorSerializer
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 # Create your views here.
 
 class BookListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     queryset =Book.objects.all()
     serializer_class = BookSerializer
 
 class BookDetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookCreateView(generics.CreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
 class BookUpdateView(generics.UpdateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookDeleteView(generics.DestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
