a9ef00fef818cb66bb0fdcbcf230056e8352e866
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-29 17:48:18 +0200

    creating api views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book , Author
+from .serializers import BookSerializer , AuthorSerializer
 
 # Create your views here.
+
+class BookListView(generics.ListAPIView):
+    queryset =Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
