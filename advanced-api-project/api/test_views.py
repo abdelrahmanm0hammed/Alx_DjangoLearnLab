0afd7389386afe376dd1e9bf590a59904457ba67
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-30 20:26:37 +0200

    creating test create_book

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -3,3 +3,21 @@ from django.urls import reverse
 from rest_framework import status
 from .models import Book
 from django.contrib.auth.models import User
+
+class BookAPITests(APITestCase):
+
+    def test_create_book(self):
+        
+        url = reverse('book-create')
+        data = {
+            "title":"mybook",
+            "author":"abood",
+            "publication_year":2025
+        }
+
+        response = self.client.post(url, data, format= 'json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 1)   
+        book = Book.objects.get()
+        self.assertEqual(book.title, "mybook")
+        self.assertEqual(book.publication_year, 2025)
