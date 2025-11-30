3ef90ac5ebf2cfee60db36a79298f4887e148200
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-11-30 20:11:10 +0200

    creating test_views.py file

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,5 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from .models import Book
+from django.contrib.auth.models import User
