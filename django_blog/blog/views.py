cf871dc5749c2965af17706f3de17eda9bba6ae4
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-12-05 17:00:19 +0200

    creating register, login and logout templates and views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+def register(request):
+
+    if request.method =='POST':
+        form =UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+        return render(request, 'blog/register.html',{'form':form})
+    
+def home(request):
+    return render(request,'blog/home.html')
+
+def posts(request):
+    return render(request,'blog/posts.html')
+
+def login(request):
+    return render(request, 'blog/login.html')
+
+def logout(request):
+    return render(request, 'blog/logout.html')
