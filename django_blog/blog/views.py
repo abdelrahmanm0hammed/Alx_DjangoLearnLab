2974898ee993ee210ac2a06e6b4c9a11b23d620a
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-12-07 14:14:35 +0200

    setting up templates for crud operations

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -41,15 +41,20 @@ def profile(request):
 
 
 class PostListView(ListView):
-   pass
+    model = Post
+    template_name='blog/post_list.html'
+    context_object_name = 'posts'
 
 
 class PostDetailView(DetailView):
-  pass
+    model = Post
+    template_name ='blog/post_detail.html'
+    context_object_name = 'post'
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model =Post
     form_class =PostForm
+    template_name='blog/post_form.html'
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
@@ -59,17 +64,28 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
    model = Post
    form_class = PostForm
+   template_name='blog/post_form.html'
 
    def form_valid(self, form):
        form.instance.author = self.request.user
        return super().form_valid(form) 
-    def test_func(self):
+   def test_func(self):
         post = self.get_object()
         return post.author == self.request.user
    
-   pass
+   
 
 
-class PostDeleteView(DeleteView):
-   pass
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+   model = Post
+   form_class = PostForm
+   template_name ='blog/post_confirm_delete.html'
+   context_object_name = 'post'
+   success_url = '/posts/'
+
+   def test_func(self):
+       post = self.get_object()
+
+       return self.request.user == post.author
+   
 
