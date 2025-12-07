a974c5f3a75a573b139df4684b5e587f4e7a36c6
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-12-07 22:33:48 +0200

    developing search functionality

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -56,20 +56,20 @@ class PostListView(ListView):
     model = Post
     template_name='blog/post_list.html'
     context_object_name = 'posts'
-    
-    def get_queryset(self):
-        queryset = super().get_queryset()
 
-        query = self.request.GET.get("q")
+def search_posts(request):
+    query = request.GET.get('q', '')
 
-        if query:
-            queryset = queryset.filter(
-                Q(title__icontains=query) |
-                Q(content__icontains=query) |
-                Q(tags__name__icontains=query)
-            ).distinct()
+    if query:
+        posts = Post.objects.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(tags__name__icontains=query)
+        ).distinct()
+    else:
+        posts = Post.objects.all()
 
-        return queryset
+    return render(request, 'blog/post_list.html', {'posts': posts, 'query': query})
 
 
 class PostDetailView(DetailView):
