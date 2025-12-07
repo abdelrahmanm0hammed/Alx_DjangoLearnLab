f0a3b5e6612d1bb3303f22b164efd5867e0f5ce0
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-12-07 22:22:49 +0200

    adding search functionality to postlistview

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
-class Tag(models.Model):
-    name =models.CharField(max_length=100)
-    def __str__(self):
-        return self.name
 
 class Post(models.Model):
     
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -10,7 +10,7 @@ from django.views.generic import CreateView, UpdateView, ListView , DeleteView, 
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
-
+from django.db.models import Q
 def register(request):
 
     if request.method =='POST':
@@ -56,6 +56,20 @@ class PostListView(ListView):
     model = Post
     template_name='blog/post_list.html'
     context_object_name = 'posts'
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        query = self.request.GET.get("q")
+
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(tags__name__icontains=query)
+            ).distinct()
+
+        return queryset
 
 
 class PostDetailView(DetailView):
