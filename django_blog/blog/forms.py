40cd795fc6da2aec1a533b598a8e9d72da609976
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-12-07 22:11:02 +0200

    adding tagwidget ot postform

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from .models import Post, Comment
+from taggit.forms import TagWidget
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
 
 
 class CommentForm(forms.ModelForm):
