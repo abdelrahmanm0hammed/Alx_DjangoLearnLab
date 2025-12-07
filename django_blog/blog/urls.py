e501a00da703a99522729d4d94f307c8dadc5e30
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-12-07 11:35:50 +0200

    creating crud views and urls for Post model

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
+from .views import(
+    PostDeleteView,
+    PostCreateView,
+    PostDetailView,
+    PostListView,
+    PostUpdateView,
+)
 
 urlpatterns = [
 
@@ -8,7 +15,12 @@ urlpatterns = [
     path('post/',views.posts,name='posts'),
     path('login/',views.login,name='login'),
     path('logout/',views.logout,name='logout'),
-    path('profile/',views.profile,name='profile')
+    path('profile/',views.profile,name='profile'),
+    path('posts/',PostListView.as_view(), name='post-list'),
+    path('posts/create/',PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostDeleteView.as_view(), name='post-detail' ),
+    path('posts/update/<int:pk>/', PostUpdateView.as_view, name='post-update'),
+    path('posts/delete/<int:pk>/', PostDeleteView.as_view, name ='post-delete'),
  
 
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from django_filters import rest_framework
 def register(request):
 
     if request.method =='POST':
@@ -30,3 +38,40 @@ def logout(request):
 
 def profile(request):
     return render(request,'blog/profile.html')
+
+
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'content', 'published_date', 'author']
+    search_fields = ['title', 'content', 'published_date', 'author']
+    ordering_fields = ['title', 'content', 'published_date', 'author']
+
+
+class PostDetailView(generics.RetrieveAPIView):
+    queryset=Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes =[IsAuthenticatedOrReadOnly]
+
+class PostCreateView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes =[TokenAuthentication]
+
+
+class PostUpdateView(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+
+class PostDeleteView(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes =[TokenAuthentication]
+
