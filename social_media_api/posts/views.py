d231e39b9fff298e35160fbe8754bead4571efaf
Abdelrahman Mohamed <abdelrahmanmuhammed04@gmail.com>
2025-12-14 17:53:52 +0200

    removing syntax error

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions, filters, 
+from rest_framework import viewsets, permissions, filters 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from rest_framework.pagination import PageNumberPagination
